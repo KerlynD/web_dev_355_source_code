1107743b7b0a757fc11d8c2aa635c0eee1402d7e
Kerlyn Difo <difokerlyn19@gmail.com>
2025-01-10 11:38:07 -0500

    day 3

---
+++ day_3/classwork/js_practice.js
@@ -0,0 +1,29 @@
+let name = 'John Doe';
+let address = '123 Main St';
+const age = 28;
+let intro = `Hello there I am $(name). I am $(age) years old. I live at $(address)`;
+
+let ssn = undefined;
+let dob = null;
+
+console.log(intro)
+// For Loops
+for(let i = 0; i < 10; i ++){
+
+}
+// While loops
+while(ssn == undefined){
+    console.log("Im undefined")
+}
+
+// Arrays
+let arr1 = []
+let arr2 = Array();
+let arr3 = ["Pop", 'Push', "Stack"];
+
+// functions
+function add(a, b){
+    return a+b
+}
+
+let subtraction = add
